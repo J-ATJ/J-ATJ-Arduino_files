29973153e35dc092b1be0121cf3d2ae385c1a8e6
Jorge Alberto Tovar Jiménez <jyoruge6798@gmail.com>
2022-09-12 16:30:08 -0500

    Addded botones

---
+++ Botones/Botones_LCD/Botones_LCD.ino
@@ -0,0 +1,35 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7); //pines que utilizaremos del arduino para conectar al lcd
+int boton = 8;
+int led = 13;
+boolean estado = HIGH;
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2); //tipo de pantalla lcd 16x2
+  pinMode(boton, INPUT);
+  pinMode(led, OUTPUT);
+  // lcd.print(" ");// imprime el texto
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(boton) != estado) {
+    estado = digitalRead(boton);
+    imprimir();
+  }
+
+}
+
+void imprimir() {
+  lcd.clear();
+  if (digitalRead(boton) == LOW) {
+    digitalWrite(led, HIGH);
+    lcd.print("Oprimido");
+  }
+  else {
+    digitalWrite(led, LOW);
+    lcd.print("Libre");
+  }
+}
+
