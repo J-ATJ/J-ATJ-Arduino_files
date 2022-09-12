446268026e5748a5748ff4b5ad9351adce3bd5e5
Jorge Alberto Tovar Jiménez <jyoruge6798@gmail.com>
2022-09-12 16:35:02 -0500

    Added RFID LCD i2C

---
+++ RFID_LCD_i2C/RFID_Con_Pantalla_LCD/RFID_Con_Pantalla_LCD.ino
@@ -0,0 +1,145 @@
+#include <SPI.h>
+#include<MFRC522.h>
+#define RST_PIN         9        
+#define SS_PIN          10       
+#include <LiquidCrystal_I2C.h>
+
+const int LEDverde = 7;
+const int LEDrojo = 6;
+
+MFRC522 RFIDJorge(SS_PIN, RST_PIN);  // Creamos Nuestro RFID 
+LiquidCrystal_I2C lcd(0x27,16,2); //iniciar lcd con i2c direccion 0x27, 
+
+// declaramos los usuarios o claves identificadas
+byte ent[4];
+byte Tarjeta[4]={0x29, 0x86, 0x64, 0x23}; //0x00 porque son hexadecimales
+byte Llavero[4]={0xFC, 0x23, 0xC2, 0x48};  // 0x49 identificado , si usamos --> 0x48 = usuario no identificado
+
+void setup() {
+  // put your setup code here, to run once: 
+  SPI.begin();             //Inicializamos SPI
+  RFIDJorge.PCD_Init();
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+
+  pinMode(LEDverde,OUTPUT);
+  pinMode(LEDrojo,OUTPUT);
+  
+        digitalWrite(LEDverde,HIGH);
+        digitalWrite(LEDrojo,HIGH);
+        delay(1000);
+        digitalWrite(LEDverde,LOW);
+        digitalWrite(LEDrojo,LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.setCursor(3,0);
+  lcd.print("Acerca tu ");
+  lcd.setCursor(0,1);
+  lcd.print(" identificacion");
+  lcd.setCursor(0,0);
+  
+  if ( ! RFIDJorge.PICC_IsNewCardPresent()) 
+  return;
+
+  if ( ! RFIDJorge.PICC_ReadCardSerial()) 
+  return;
+  
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(" Tu clave:");
+  lcd.setCursor(0,1);
+
+  for(byte i=0; i<RFIDJorge.uid.size; i++){
+  
+    if (RFIDJorge.uid.uidByte[i]<0x10){
+       lcd.print(" 0");
+      }
+      else{
+          lcd.print(" ");
+        }
+   
+      
+    lcd.print(RFIDJorge.uid.uidByte[i],HEX);
+    ent[i]=RFIDJorge.uid.uidByte[i];  
+    }
+    
+    delay(4000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    
+    if(verificarT(ent,Tarjeta)){
+    lcd.print("   Bienvenido");
+    lcd.setCursor(3,1);
+    lcd.print(" Tarjeta");
+    digitalWrite(LEDverde,HIGH);
+    delay(2000);
+    digitalWrite(LEDverde,LOW);
+    }
+    
+    else if(verificarL(ent,Llavero)){
+      lcd.print("   Bienvenido");
+      lcd.setCursor(3,1);
+      lcd.print(" Llavero");
+      digitalWrite(LEDverde,HIGH);
+      delay(2000);
+      digitalWrite(LEDverde,LOW);
+    }
+    else {
+             lcd.setCursor(1,0);
+             lcd.print(" Usuario no ");
+             lcd.setCursor(2,1);
+             lcd.print("Registrado "); 
+             lcd.clear();
+             lcd.print("NO hay acceso :(");
+             digitalWrite(LEDrojo,HIGH);
+             delay(2000);
+             digitalWrite(LEDrojo,LOW);
+            
+    }
+    
+    RFIDJorge.PICC_HaltA();
+    delay(1000);
+    lcd.clear();
+  }
+
+  boolean verificarT(byte ent[],byte Tarjeta[])
+  {
+    for (byte i=0;i<RFIDJorge.uid.size;i++){
+    if (ent[i] != Tarjeta[i])
+    return(false);
+    }
+    return (true);
+    }
+  
+  boolean verificarL(byte ent[],byte Llavero[])
+  {
+    for (byte i=0;i<RFIDJorge.uid.size;i++){
+    if (ent[i] != Llavero[i])
+    return(false);
+    }
+    return (true);
+    }
+
+  /* RFID 3.3v
+   *  
+   *  Typical pin layout used:
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ * 
+ * Para i2C 
+ * sda=pin a4
+ * scl= pin a5
+ *  vcc=5v
+ */
+
---
+++ RFID_LCD_i2C/Scanner_i2C/Scanner_i2C.ino
@@ -0,0 +1,77 @@
+#include <deprecated.h>
+#include <MFRC522.h>
+#include <MFRC522Extended.h>
+#include <require_cpp11.h>
+
+// 
+// i2c_scanner
+//
+// Version 1
+// Este programa (código que aparece) se pueden encontrar en muchos sitios.
+// Por ejemplo, en el foro Arduino.cc.
+// El autor original no se conoce.
+// Version 2, Juni 2012, Using Arduino 1.0.1
+// Adaptado para ser lo más simple posible por usuario Krodal Arduino.cc
+// Version 3, Feb 26 2013 by louarnold
+// Version 4, March 3, 2013, Using Arduino 1.0.3
+// by Arduino.cc user Krodal.
+// Version 5, March 28, 2013
+// Como la versión 4, pero las exploraciones de direcciones ahora a 127. 
+//
+// Este boceto prueba las direcciones estándar de 7 bits. 
+// Los dispositivos con dirección de bits más alta 
+// no pueden ser vistas correctamente.
+ 
+#include "Wire.h" ; si no aparece la librería es por motivos del soft "< "
+ 
+void setup()
+{
+Wire.begin();
+ 
+Serial.begin(9600);
+Serial.println("\nI2C Scanner");
+}
+ 
+void loop()
+{
+byte error, address;
+int nDevices;
+ 
+Serial.println("Scanning...");
+ 
+nDevices = 0;
+for(address = 1; address < 127; address++ ) 
+{
+// El i2c_scanner utiliza el valor devuelto o el 
+// Write.endTransmisstion para ver si un 
+// dispositivo reconoció la dirección.
+Wire.beginTransmission(address);
+error = Wire.endTransmission();
+ 
+if (error == 0)
+{
+Serial.print("Dispositivo I2C en dirección 0x");
+if (address<16) 
+Serial.print("0");
+Serial.print(address,HEX);
+Serial.println(" !");
+ 
+nDevices++;
+}
+else if (error==4) 
+{
+Serial.print("Error desconocido en dirección 0x");
+if (address<16) 
+Serial.print("0");
+Serial.println(address,HEX);
+} 
+}
+if (nDevices == 0)
+Serial.println("Dispositivo I2C No hallado.\n");
+else
+Serial.println("hecho\n");
+ 
+delay(5000);   // espera 5 segundos para otro scan
+}
+
+//Copie y pegue este código compile y súbalo a su Arduino. Ahora, abra la ventana del monitor serie, para ver los m
