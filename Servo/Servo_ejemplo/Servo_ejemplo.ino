a56075e4be1006d1535374ab902a534a191f980b
Jorge Alberto Tovar Jiménez <jyoruge6798@gmail.com>
2022-09-12 16:30:58 -0500

    Added servo

---
+++ Servo/Servo_ejemplo/Servo_ejemplo.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+Servo pluma;
+int senhal=1;
+
+int pulso_minimo = 1000;
+int pulso_maximo = 2000;
+
+void setup() {
+  // put your setup code here, to run once:
+  pluma.attach(senhal, pulso_minimo, pulso_maximo); // señal del servo en el pin 9 (senhal)
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+pluma.write(0);
+delay(3000);
+pluma.write(100);
+delay(3000);
+}
