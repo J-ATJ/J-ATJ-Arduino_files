4faa9c50cf051696422013a404d0e0c1790448a0
Jorge Alberto Tovar Jiménez <jyoruge6798@gmail.com>
2022-09-12 16:27:46 -0500

    Added sensor ultrasónico

---
+++ Sensor_Ultrasonico/Distancia_con_parpadeo_de_led/Distancia_con_parpadeo_de_led.ino
@@ -0,0 +1,27 @@
+const int trig = 8;
+const int echo = 7;
+int tiempo;
+int distancia;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trig, HIGH);
+  delay(1);
+  digitalWrite(trig, LOW);
+  tiempo = pulseIn(echo, HIGH);
+  distancia = tiempo / 58; 
+  Serial.println(distancia);
+  delay(100);
+  if (distancia <= 30 && distancia >= 0) {
+    digitalWrite(13, HIGH);
+    delay(distancia * 10);
+    digitalWrite(13, LOW);
+  }
+}
